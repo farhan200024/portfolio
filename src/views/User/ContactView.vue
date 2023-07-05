0331a9c2093d24e39e56ba93f935558074a79b1f
Farhan Muhammad <47453676+farhan200024@users.noreply.github.com>
2023-07-05 20:00:18 +0100

    sends email

--- src/views/User/ContactView.vue
+++ src/views/User/ContactView.vue
@@ -1,27 +1,72 @@
 <template>
-  <div class="home">
-  <h1>Home</h1>
-  <span v-if="adminName != ''">Admin cant fill in this form.</span>
-  <br><br>
-  <form :class="{'disabled': adminName != ''}">
-  <label for="name">name</label>
-  <input name="name" v-model="name">
-  </form>
-  </div>
+    <div class="home">
+      <h1>Home</h1>
+      <span v-if="adminName != ''">Admin cant fill in this form.</span>
+        <form @submit.prevent="sendEmail" :class="{'disabled': adminName != ''}">
+          <label>Name</label>
+          <input 
+            type="text" 
+            v-model="name"
+            name="name"
+            placeholder="Your Name"
+          >
+          <label>Email</label>
+          <input 
+            type="email" 
+            v-model="email"
+            name="email"
+            placeholder="Your Email"
+            >
+          <label>Message</label>
+          <textarea 
+            name="message"
+            v-model="message"
+            cols="30" rows="5"
+            placeholder="Message">
+          </textarea>
+          
+          <input type="submit" value="Send">
+        </form>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import emailjs from 'emailjs-com';
 export default {
   name: 'HomeView',
   data() {
     return {
+      name: '',
+      email: '',
+      message: '',
       adminName: this.$store.state.adminName,
-      name     : ''
-    };
+    }
   },
   methods: {
+    sendEmail: async function (e) {
+      try {
+        const a = await emailjs.sendForm('service_o7uf7aj', 'template_n0l1ak8', e.target, 'XOh-_5_Jiui_BnPao', {
+          name: this.name,
+          email: this.email,
+          message: this.meessage
+        })
+
+        if (a.status == 200 && a.text == 'OK') {
+          alert('email sent');
+        }
+
+      } catch (err) {
+           if (err instanceof ReferenceError) {
+            alert( "JSON Error: " + err.message );
+           } else {
+            throw err; // rethrow error
+           }
+      }
+
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
     created(){
       this.adminName = this.$store.state.adminName
     }
